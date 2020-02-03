534cddaa7da87e7446526803924edf4198778e41
Panchajanya1999 <panchajanya@azure-dev.live>
2020-02-03 13:23:33 +0530

    cpufreq: schedutil: Expose limit tunables cluster-wise
    This allows us to tune the default rate-limits for each cluster
    without constantly changing schedutil's code.
    
    Idea taken from : https://github.com/kdrag0n/proton_zf6/commit/d9faa70d8f803c87663118dcba1f5205f658b557#diff-d96c15a8e1af1ce4aa4750bef6b7e38d
    [panchajanya1999] : Allow tuning for each cluster
    
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    (cherry picked from commit de92c2b28b07344599f2e4104a6f149f90bf4f24)
    Change-Id: I9d36a99dddc2a7138f44336d4325930c28425220
    Signed-off-by: Harish <sanganiharish263@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -447,6 +447,7 @@ static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
+	/* Apply init protection, else values will get overwritten */
 	if (task_is_booster(current))
 		return count;
 
@@ -470,6 +471,7 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
+	/* Apply init protection, else values will get overwritten */
 	if (task_is_booster(current))
 		return count;
 
@@ -682,6 +684,20 @@ static int sugov_init(struct cpufreq_policy *policy)
 
 	tunables->up_rate_limit_us = UP_RATE_LIMIT_US;
 	tunables->down_rate_limit_us = DOWN_RATE_LIMIT_US;
+
+	if (cpumask_test_cpu(policy->cpu, cpu_perf_mask)) {
+		tunables->up_rate_limit_us =
+					CONFIG_SCHEDUTIL_UP_RATE_LIMIT_BIG;
+		tunables->down_rate_limit_us =
+					CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT_BIG;
+	}
+
+	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask)) {
+		tunables->up_rate_limit_us =
+					CONFIG_SCHEDUTIL_UP_RATE_LIMIT_LITTLE;
+		tunables->down_rate_limit_us =
+					CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT_LITTLE;
+	}
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
