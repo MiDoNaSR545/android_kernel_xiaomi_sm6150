bc3a5510fa77ad5574b6a38dfecb8ac7f4444173
Vincent Guittot <vincent.guittot@linaro.org>
2020-09-21 09:24:21 +0200

    sched/fair: Relax constraint on task's load during load balance
    Some UCs like 9 always running tasks on 8 CPUs can't be balanced and the
    load balancer currently migrates the waiting task between the CPUs in an
    almost random manner. The success of a rq pulling a task depends of the
    value of nr_balance_failed of its domains and its ability to be faster
    than others to detach it. This behavior results in an unfair distribution
    of the running time between tasks because some CPUs will run most of the
    time, if not always, the same task whereas others will share their time
    between several tasks.
    
    Instead of using nr_balance_failed as a boolean to relax the condition
    for detaching task, the LB will use nr_balanced_failed to relax the
    threshold between the tasks'load and the imbalance. This mecanism
    prevents the same rq or domain to always win the load balance fight.
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Phil Auld <pauld@redhat.com>
    Link: https://lkml.kernel.org/r/20200921072424.14813-2-vincent.guittot@linaro.org
    Signed-off-by: shygosh <shygosh@proton.me>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9635,8 +9635,7 @@ redo:
 		if (sched_feat(LB_MIN) && load < 16 && !env->sd->nr_balance_failed)
 			goto next;
 
-		if (((load / 2) > env->imbalance &&
-		    env->sd->nr_balance_failed <= env->sd->cache_nice_tries))
+		if ((load >> env->sd->nr_balance_failed) > env->imbalance)
 			goto next;
 
 		detach_task(p, env);
