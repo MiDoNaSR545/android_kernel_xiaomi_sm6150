75d3217f9baf3a8e648bed9d09df6809112cf7fa
darkhz <kmachanwenw@gmail.com>
2021-02-10 13:20:08 +0530

    drivers: Disable debug messages
    Signed-off-by: neophyte <neophyteplayground@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -32,6 +32,8 @@
 #include "exposure_adjustment.h"
 #endif
 
+#include <linux/printk_disable_msg.h>
+
 /**
  * topology is currently defined by a set of following 3 values:
  * 1. num of layer mixers
