f18828822c0a57321376924438078f9231c84e5d
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2024-01-04 17:03:19 +0700

    ti: pd_policy_manager: Check once to detect ln8000 charger
    It's enough to check the LN8000 charger once on the probe, and use global
    variables without using the pdpm struct to separate the multiple
    configurations needed for the LN8000 and BQ2597X chargers.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- drivers/power/supply/ti/pd_policy_manager.c
+++ drivers/power/supply/ti/pd_policy_manager.c
@@ -91,6 +91,7 @@ static int fc2_taper_timer;
 static int cool_overcharge_timer;
 static int ibus_lmt_change_timer;
 
+static bool ln8000_is_valid = false;
 
 static void usbpd_check_usb_psy(struct usbpd_pm *pdpm)
 {
@@ -390,7 +391,7 @@ static void usbpd_check_cp_psy(struct usbpd_pm *pdpm)
     }
 }
 
-static bool usbpd_check_ln8000_chg(struct usbpd_pm *pdpm)
+static void usbpd_check_ln8000_chg(struct usbpd_pm *pdpm)
 {
 	int rc;
 	union power_supply_propval val;
@@ -399,15 +400,15 @@ static bool usbpd_check_ln8000_chg(struct usbpd_pm *pdpm)
 				POWER_SUPPLY_PROP_MODEL_NAME, &val);
 	if (rc < 0) {
 		pr_err("Failed getting charger IC name, rc=%d\n", rc);
-		return false;
+		ln8000_is_valid = false;
 	}
 
 	if (strcmp(val.strval, "ln8000") == 0) {
 		pr_info("Detected ln8000 IC charger\n");
-		return true;
+		ln8000_is_valid = true;
 	} else {
 		pr_info("Detected other IC charger\n");
-		return false;
+		ln8000_is_valid = false;
 	}
 }
 
@@ -995,7 +996,7 @@ static int usbpd_pm_fc2_charge_algo(struct usbpd_pm *pdpm)
 
 	//usbpd_set_new_fcc_voter(pdpm);
 
-	if (usbpd_check_ln8000_chg(pdpm)) {
+	if (ln8000_is_valid) {
 		taper_timeout = 25000 / 500;
 		ibus_timeout = 2500 / 500;
 	} else {
@@ -1040,7 +1041,7 @@ static int usbpd_pm_fc2_charge_algo(struct usbpd_pm *pdpm)
 		ibus_lmt_change_timer = 0;
 	}
 
-	if (!usbpd_check_ln8000_chg(pdpm))
+	if (!ln8000_is_valid)
 		ibus_limit = min(ibus_limit, pdpm->apdo_max_curr);
 
 	pr_debug("chg_mode:%d, curr_ibus_limit:%d, ibus_limit:%d, bat_curr_lp_lmt:%d, effective_fcc_val:%d, apdo_max_curr:%d\n",
@@ -1167,7 +1168,7 @@ static int usbpd_pm_fc2_charge_algo(struct usbpd_pm *pdpm)
 	pr_debug("steps: %d, sw_ctrl_steps:%d, hw_ctrl_steps:%d\n", steps, sw_ctrl_steps, hw_ctrl_steps);
 	pdpm->request_voltage += steps * STEP_MV;
 
-	if (usbpd_check_ln8000_chg(pdpm))
+	if (ln8000_is_valid)
 		pdpm->request_current = min(pdpm->apdo_max_curr, curr_ibus_limit);
 
 	if (pdpm->apdo_max_volt == PPS_VOL_MAX)
@@ -1180,7 +1181,7 @@ static int usbpd_pm_fc2_charge_algo(struct usbpd_pm *pdpm)
 			&& pdpm->request_voltage > pdpm->adapter_voltage + 500)
 		pdpm->request_voltage = pdpm->adapter_voltage + 500; */
 
-	if (!usbpd_check_ln8000_chg(pdpm))
+	if (!ln8000_is_valid)
 		pdpm->request_current = min(pdpm->apdo_max_curr, curr_ibus_limit);
 
 	pr_debug("steps:%d, pdpm->request_voltage:%d, pdpm->request_current:%d\n",
@@ -1231,7 +1232,7 @@ static int usbpd_pm_sm(struct usbpd_pm *pdpm)
 		pdpm->is_temp_out_fc2_range = pd_disable_cp_by_jeita_status(pdpm);
 		pr_debug("is_temp_out_fc2_range:%d\n", pdpm->is_temp_out_fc2_range);
 
-		if (usbpd_check_ln8000_chg(pdpm))
+		if (ln8000_is_valid)
 			pd_get_batt_capacity(pdpm, &capacity);
 
 		effective_fcc_val = usbpd_get_effective_fcc_val(pdpm);
@@ -1248,7 +1249,7 @@ static int usbpd_pm_sm(struct usbpd_pm *pdpm)
 		if (pdpm->cp.vbat_volt < pm_config.min_vbat_for_cp) {
 			pr_debug("batt_volt %d, waiting...\n", pdpm->cp.vbat_volt);
 		} else if ((pdpm->cp.vbat_volt > pm_config.bat_volt_lp_lmt - 50)
-			|| (capacity >= 89 && usbpd_check_ln8000_chg(pdpm))) {
+			|| (capacity >= 89 && ln8000_is_valid)) {
 			pr_debug("batt_volt %d is too high for cp, charging with switch charger\n",
 					pdpm->cp.vbat_volt);
 			usbpd_pm_move_state(pdpm, PD_PM_STATE_FC2_EXIT);
@@ -1296,7 +1297,7 @@ static int usbpd_pm_sm(struct usbpd_pm *pdpm)
 			pdpm->request_current = min(pdpm->apdo_max_curr, curr_ibus_lmt);
 			pdpm->request_current = min(pdpm->request_current, MAX_BYPASS_CURRENT_MA);
 		} else {
-			if (!usbpd_check_ln8000_chg(pdpm))
+			if (!ln8000_is_valid)
 			 curr_ibus_lmt = curr_fcc_lmt >> 1;
 			pdpm->request_voltage = pdpm->cp.vbat_volt * 2 + BUS_VOLT_INIT_UP;
 			pdpm->request_current = min(pdpm->apdo_max_curr, curr_ibus_lmt);
@@ -1313,7 +1314,7 @@ static int usbpd_pm_sm(struct usbpd_pm *pdpm)
 
 	case PD_PM_STATE_FC2_ENTRY_2:
 #ifdef CONFIG_K6_CHARGE
-	if (usbpd_check_ln8000_chg(pdpm)) {	
+	if (ln8000_is_valid) {	
 		pr_debug("bus_err_st:%d, req_vol:%dmV, cur_vol:%d, req_curr:%d, vbat:%d, retry:%d\n",
 				pdpm->cp.bus_error_status, pdpm->request_voltage, pdpm->cp.vbus_volt, pdpm->request_current, pdpm->cp.vbat_volt, tune_vbus_retry);
 		if (pdpm->cp.bus_error_status == VBUS_ERROR_LOW ||
@@ -1450,7 +1451,7 @@ static int usbpd_pm_sm(struct usbpd_pm *pdpm)
 			pr_debug("Slow Charging Feature is running %d\n", ret);
 			usbpd_pm_move_state(pdpm, PD_PM_STATE_FC2_EXIT);
 		} else {
-			if (usbpd_check_ln8000_chg(pdpm)) {
+			if (ln8000_is_valid) {
 				usbpd_select_pdo(pdpm->pd, pdpm->apdo_selected_pdo,
 						pdpm->request_voltage * 1000,
 						pdpm->request_current * 1000);
@@ -1538,9 +1539,9 @@ static void usbpd_pm_workfunc(struct work_struct *work)
 			__func__, pm_config.bat_volt_lp_lmt, pdpm->cp.vbat_volt);
 
 	if (!usbpd_pm_sm(pdpm) && pdpm->pd_active) {
-		if (pdpm->state == PD_PM_STATE_FC2_ENTRY_2 && usbpd_check_ln8000_chg(pdpm)) {
+		if (pdpm->state == PD_PM_STATE_FC2_ENTRY_2 && ln8000_is_valid) {
 			interval = 200;
-		} else if (usbpd_check_ln8000_chg(pdpm)) {
+		} else if (ln8000_is_valid) {
 			interval = 500;
 		} else {
 			interval = PM_WORK_RUN_INTERVAL;
@@ -1801,6 +1802,7 @@ static int usbpd_pm_probe(struct platform_device *pdev)
 	spin_lock_init(&pdpm->psy_change_lock);
 
 	usbpd_check_cp_psy(pdpm);
+	usbpd_check_ln8000_chg(pdpm);
 	usbpd_check_cp_sec_psy(pdpm);
 	usbpd_check_usb_psy(pdpm);
 
