9eb944d623ab82c2eed1b7734dbbed18b36eed97
shygosh <shygosh@proton.me>
2024-10-12 00:56:45 +0700

    sched/cass: Check for sync wakes first
    Signed-off-by: shygosh <shygosh@proton.me>
    Signed-off-by: RainySorcerer <beastdark704@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -97,12 +97,12 @@ bool cass_cpu_better(const struct cass_cpu_cand *a,
 		     fits_capacity(p_util, b->cap_max)))
 		goto done;
 
-	/* Prefer the CPU with lower relative utilization */
-	if (cass_cmp(b->util, a->util))
-		goto done;
-
 	/* Prefer the current CPU for sync wakes */
 	if (sync && (cass_eq(a->cpu, this_cpu) || !cass_cmp(b->cpu, this_cpu)))
+		goto done;
+
+	/* Prefer the CPU with lower relative utilization */
+	if (cass_cmp(b->util, a->util))
 		goto done;
 
 	/* Prefer the CPU with higher capacity */
