1b43bcc32df3096dd4a1cce010bf9ddbe85d8f56
K Prateek Nayak <kprateek.nayak@amd.com>
2024-11-28 12:59:54 +0530

    sched/fair: Fix NEXT_BUDDY
    Adam reports that enabling NEXT_BUDDY insta triggers a WARN in
    pick_next_entity().
    
    Moving clear_buddies() up before the delayed dequeue bits ensures
    no ->next buddy becomes delayed. Further ensure no new ->next buddy
    ever starts as delayed.
    
    Fixes: 152e11f6df29 ("sched/fair: Implement delayed dequeue")
    Reported-by: Adam Li <adamli@os.amperecomputing.com>
    Signed-off-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Adam Li <adamli@os.amperecomputing.com>
    Link: https://lkml.kernel.org/r/670a0d54-e398-4b1f-8a6e-90784e2fdf89@amd.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4904,6 +4904,7 @@ dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 	 * Update run-time statistics of the 'current'.
 	 */
 	update_curr(cfs_rq);
+	clear_buddies(cfs_rq, se);
 
 	if (flags & DEQUEUE_DELAYED) {
 		SCHED_WARN_ON(!se->sched_delayed);
@@ -4920,8 +4921,6 @@ dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 
 		if (sched_feat(DELAY_DEQUEUE) && delay &&
 		    !entity_eligible(cfs_rq, se)) {
-			if (cfs_rq->next == se)
-				cfs_rq->next = NULL;
 			update_load_avg(cfs_rq, se, 0);
 			se->sched_delayed = 1;
 			return false;
@@ -4940,8 +4939,6 @@ dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 	dequeue_runnable_load_avg(cfs_rq, se);
 
 	update_stats_dequeue(cfs_rq, se, flags);
-
-	clear_buddies(cfs_rq, se);
 
 	update_entity_lag(cfs_rq, se);
 	if (sched_feat(PLACE_REL_DEADLINE) && !sleep) {
@@ -9345,6 +9342,9 @@ static void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_
 	if (unlikely(throttled_hierarchy(cfs_rq_of(pse))))
 		return;
 
+	if (sched_feat(NEXT_BUDDY) && !(wake_flags & WF_FORK) && !pse->sched_delayed) {
+		set_next_buddy(pse);
+	}
 	/*
 	 * We can come here with TIF_NEED_RESCHED already set from new task
 	 * wake up path.
