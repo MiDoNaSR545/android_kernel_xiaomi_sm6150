6c1ba7e161b6acefba0d259b334ebb8022ee1bc2
Qun-Wei Lin <qun-wei.lin@mediatek.com>
2025-04-30 16:26:41 +0800

    mm: Add Kcompressd for accelerated memory compression
    [FROMLIST]
    
    This patch series introduces a new mechanism called kcompressd to
    improve the efficiency of memory reclaiming in the operating system.
    
    Problem:
      In the current system, the kswapd thread is responsible for both scanning
      the LRU pages and handling memory compression tasks (such as those
      involving ZSWAP/ZRAM, if enabled). This combined responsibility can lead
      to significant performance bottlenecks, especially under high memory
      pressure. The kswapd thread becomes a single point of contention, causing
      delays in memory reclaiming and overall system performance degradation.
    
    Solution:
      Introduced kcompressd to handle asynchronous compression during memory
      reclaim, improving efficiency by offloading compression tasks from
      kswapd. This allows kswapd to focus on its primary task of page reclaim
      without being burdened by the additional overhead of compression.
    
    In our handheld devices, we found that applying this mechanism under high
    memory pressure scenarios can increase the rate of pgsteal_anon per second
    by over 260% compared to the situation with only kswapd. Additionally, we
    observed a reduction of over 50% in page allocation stall occurrences,
    further demonstrating the effectiveness of kcompressd in alleviating memory
    pressure and improving system responsiveness.
    
    Co-developed-by: Barry Song <21cnbao@gmail.com>
    Signed-off-by: Barry Song <21cnbao@gmail.com>
    Signed-off-by: Qun-Wei Lin <qun-wei.lin@mediatek.com>
    Reference: Re: [PATCH 0/2] Improve Zram by separating compression context from kswapd - Barry Song
               https://lore.kernel.org/lkml/20250313093005.13998-1-21cnbao@gmail.com/
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -18,6 +18,7 @@
 #include <linux/pageblock-flags.h>
 #include <linux/page-flags-layout.h>
 #include <linux/atomic.h>
+#include <linux/kfifo.h>
 #include <asm/page.h>
 
 /* Free memory management - zoned buddy allocator.  */
@@ -900,6 +901,11 @@ typedef struct pglist_data {
 
 	wait_queue_head_t kshrinkd_wait;
 	struct task_struct *kshrinkd;
+
+#define KCOMPRESS_FIFO_SIZE 256
+	wait_queue_head_t kcompressd_wait;
+	struct task_struct *kcompressd;
+	struct kfifo kcompress_fifo;
 
 #ifdef CONFIG_COMPACTION
 	int kcompactd_max_order;
