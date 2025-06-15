9cc2107953b84107da7e73303021e626fa181b25
Yahya Wessam <yahyawessam2002@gmail.com>
2025-06-15 16:02:05 +0300

    {init, zram_drv}: allow overriding zram size from cmdline interface
    This is a refactored verison of [1] but without a Kconfig entry, it instead adds a cmdline interface over at init to read the value to override from; inspired by [2]
    
    [1]: https://github.com/vantoman/kernel_xiaomi_sm6150/commit/ded83187e2e9852e14f7599a857f1990ed8e1294
    [2]: https://github.com/vbajs/android_kernel_xiaomi_sweet/commit/7cd2f8fe16acc5cbc318817406f61c79eb738ad9
    
    Co-authored-by: Yaroslav Furman <yaro330@gmail.com>
    Co-authored-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- init/main.c
+++ init/main.c
@@ -102,6 +102,16 @@ static int kernel_init(void *);
 extern void init_IRQ(void);
 extern void radix_tree_init(void);
 
+int zram_size_override = 0;
+EXPORT_SYMBOL(zram_size_override);
+
+static int __init read_zram_size_override(char *s)
+{
+	kstrtoint(s, 10, &zram_size_override);
+	return 1;
+}
+__setup("override.zram_size=", read_zram_size_override);
+
 /*
  * Debug helper: via this flag we know that we are in 'early bootup code'
  * where only the boot processor is running with IRQ disabled.  This means
