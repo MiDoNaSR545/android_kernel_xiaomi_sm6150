dereference23/kernel_xiaomi_sm6375

lpm-levels: Expose `sleep_disabled`
Change-Id: Ide920ba562d096cd7d29efc3d09bcb71a7ecbdcf
Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

include/linux/cpuidle.h
@@ -307,4 +307,13 @@ static inline int cpuidle_register_governor(struct cpuidle_governor *gov)
 #define CPU_PM_CPU_IDLE_ENTER_RETENTION_PARAM(low_level_idle_enter, idx, state)	\
 	__CPU_PM_CPU_IDLE_ENTER(low_level_idle_enter, idx, state, 1)
 
+#ifdef CONFIG_ARM_QCOM_LPM_CPUIDLE
+extern bool lpm_sleep_disabled(void);
+#else
+static bool lpm_sleep_disabled(void)
+{
+	return false;
+}
+#endif
+
 #endif /* _LINUX_CPUIDLE_H */

MiDoNaSR545/android_kernel_xiaomi_sm6150

Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

include/linux/cpuidle.h
@@ -290,4 +290,13 @@ static inline void cpuidle_set_idle_cpu(unsigned int cpu) { }
 static inline void cpuidle_clear_idle_cpu(unsigned int cpu) { }
 #endif
 
+#ifdef CONFIG_MSM_PM
+extern bool lpm_sleep_disabled(void);
+#else
+static bool lpm_sleep_disabled(void)
+{
+	return false;
+}
+#endif
+
 #endif /* _LINUX_CPUIDLE_H */