0f43e3b62f7350710eefc3b46584678a970aedee
Yahya Wessam <yahyawessam2002@gmail.com>
2025-11-01 21:32:17 +0200

    Revert "cpufreq: Force switch to schedhorizon governor when profiles in on level 3"
    This reverts commit b38a140db361e5edd7a2856be0516dc147fde822.
    
    It only modifies the output of what governor is used, but the governor remains what it was before (in our default case, schedutil)
    
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -746,11 +746,7 @@ static ssize_t show_scaling_governor(struct cpufreq_policy *policy, char *buf)
 #ifdef CONFIG_KPROFILES
 	else if (kp_active_mode() == 1)
 		return sprintf(buf, "powersave\n");
-#ifdef CONFIG_CPU_FREQ_GOV_SCHEDHORIZON
-	else if (kp_active_mode() == 3)
-		return sprintf(buf, "schedhorizon\n");
-#endif // CONFIG_CPU_FREQ_GOV_SCHEDHORIZON
-#endif // CONFIG_KPROFILES
+#endif
 	else if (policy->governor)
 		return scnprintf(buf, CPUFREQ_NAME_PLEN, "%s\n",
 				policy->governor->name);
