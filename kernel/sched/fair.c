c0f632f2abaa6c9dfc48fa2e9bff681c9429928b
Peter Zijlstra <peterz@infradead.org>
2025-11-07 17:01:24 +0100

    sched/fair: Small cleanup to sched_balance_newidle()
    Pull out the !sd check to simplify code.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Tested-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Tested-by: Chris Mason <clm@meta.com>
    Link: https://patch.msgid.link/20251107161739.525916173@infradead.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -11612,16 +11612,20 @@ static int idle_balance(struct rq *this_rq, struct rq_flags *rf)
 	 */
 	rq_unpin_lock(this_rq, rf);
 
-	if (!force_lb && (this_rq->avg_idle < sysctl_sched_migration_cost ||
-	    !READ_ONCE(this_rq->rd->overload))) {
-		rcu_read_lock();
-		sd = rcu_dereference_check_sched_domain(this_rq->sd);
-		if (sd)
-			update_next_balance(sd, &next_balance);
+	rcu_read_lock();
+	sd = rcu_dereference_check_sched_domain(this_rq->sd);
+	if (!sd) {
 		rcu_read_unlock();
-
 		goto out;
 	}
+
+	if (!force_lb && (!READ_ONCE(this_rq->rd->overload) ||
+	    (this_rq->avg_idle < sd->max_newidle_lb_cost))) {
+		update_next_balance(sd, &next_balance);
+		rcu_read_unlock();
+		goto out;
+	}
+	rcu_read_unlock();
 
 	raw_spin_unlock(&this_rq->lock);
 
