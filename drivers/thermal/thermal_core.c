yashlearnpython/kernel_xiaomi_vayu

thermal: Turn down unused debug info
Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

drivers/thermal/thermal_core.c
@@ -1849,11 +1849,11 @@ static int screen_state_for_thermal_callback(struct notifier_block *nb, unsigned
 	switch (blank) {
 	case MSM_DRM_BLANK_POWERDOWN:
 		sm.screen_state = 0;
-		pr_warn("%s: DRM_BLANK_POWERDOWN\n", __func__);
+		pr_debug("%s: DRM_BLANK_POWERDOWN\n", __func__);
 		break;
 	case MSM_DRM_BLANK_UNBLANK:
 		sm.screen_state = 1;
-		pr_warn("%s: DRM_BLANK_UNBLANK\n", __func__);
+		pr_debug("%s: DRM_BLANK_UNBLANK\n", __func__);
 		break;
 	default:
 		break;

MiDoNaSR545/android_kernel_xiaomi_sm6150

Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

drivers/thermal/thermal_core.c
@@ -1921,11 +1921,11 @@ static int screen_state_for_thermal_callback(struct notifier_block *nb, unsigned
 	switch (blank) {
 	case MSM_DRM_BLANK_POWERDOWN:
 		sm.screen_state = 0;
-		pr_warn("%s: MSM_DRM_BLANK_POWERDOWN\n", __func__);
+		pr_debug("%s: MSM_DRM_BLANK_POWERDOWN\n", __func__);
 		break;
 	case MSM_DRM_BLANK_UNBLANK:
 		sm.screen_state = 1;
-		pr_warn("%s: MSM_DRM_BLANK_UNBLANK\n", __func__);
+		pr_debug("%s: MSM_DRM_BLANK_UNBLANK\n", __func__);
 		break;
 	default:
 		break;