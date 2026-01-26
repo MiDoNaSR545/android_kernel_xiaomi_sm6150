1434e35428b74f182f2b604834820b803fea2181
basamaryan <basam.aryan@gmail.com>
2026-01-26 02:12:42 -0800

    syscall: Bump BPF override to 5.10.247
    This matches android12-5.10
    
    Test: atest netd_integration_test
    Change-Id: I5e528f630ee47d396bee8dac86938f64992406d6
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    [vbajs: Adapt for bbc0d1833bbc6d50f5e5e3a74bc2c7efd42a0232 ("uname: Add support for fake uname")]
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1201,7 +1201,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 #elif defined(CONFIG_FAKE_UNAME_5_4)
 				strcpy(tmp.release, "5.4.299");
 #elif defined(CONFIG_FAKE_UNAME_5_10)
-				strcpy(tmp.release, "5.10.200");
+				strcpy(tmp.release, "5.10.247");
 #elif defined(CONFIG_FAKE_UNAME_5_15)
 				strcpy(tmp.release, "5.15.200");
 #elif defined(CONFIG_FAKE_UNAME_6_1)
