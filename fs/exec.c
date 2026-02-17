42e4332f1f8c68ee3763c9dc06acc8010959c20f
Yahya Wessam <yahyawessam2002@gmail.com>
2026-02-18 01:14:32 +0200

    [SQUASH] af_unix: drop non-working perf logcat silencer
    It still spams logcat.
    
    Revert "fs: Block various userspace bloatware services from running"
    
    This reverts commit 9aec5db14abcb80c363373317a0f8c6f099652a3.
    
    Revert "Reapply "devfreq: Prevent boosters from clamping the max KGSL frequency""
    
    This reverts commit 9a4b896f2cc61287d274565fa580eca0037a66db.
    
    Revert "af_unix: Extend powerhal blocking list"
    
    This reverts commit 60bbd6de3b9841db59752ec4d94d6172fd9b42e1.
    
    Revert "af_unix: Block PERFD from writing to the logd socket"
    
    This reverts commit 4442d3b614c19e9a93016fffb06be2267a494f47.
    
    Revert "af_unix: Block libperfmgr from writing to the logd socket"
    
    This reverts commit 12d121d66070d7cb2a212cfb87e4e3e8aeb5f232.
    
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -74,41 +74,6 @@
 
 int suid_dumpable = 0;
 
-#define SERVICEMANAGER_BIN "/system/bin/servicemanager"
-#define LIBPERFMGR "/vendor/bin/hw/android.hardware.power-service.xiaomi-libperfmgr"
-#define LIBPERFMGR_BIN "/vendor/bin/hw/android.hardware.power-service.lineage-libperfmgr"
-#define PERF "/vendor/bin/hw/vendor.qti.hardware.perf-hal-service"
-#define PERFD "/vendor/bin/hw/vendor.qti.hardware.perf2-hal-service"
-#define PERFH "/vendor/bin/hw/vendor.qti.hardware.perf@2.2-service"
-#define IOP "/vendor/bin/hw/vendor.qti.hardware.iop@2.0-service"
-
-static struct task_struct *powerhal_tsk;
-bool task_is_powerhal(struct task_struct *p)
-{
-	struct task_struct *tsk;
-	bool ret;
-
-	rcu_read_lock();
-	tsk = READ_ONCE(powerhal_tsk);
-	ret = tsk && same_thread_group(p, tsk);
-	rcu_read_unlock();
-
-	return ret;
-}
-
-static struct task_struct *servicemanager_tsk;
-bool task_is_servicemanager(struct task_struct *p)
-{
-	return p == READ_ONCE(servicemanager_tsk);
-}
-
-void dead_special_task(void)
-{
-	if (unlikely(current == powerhal_tsk))
-		WRITE_ONCE(servicemanager_tsk, NULL);
-		WRITE_ONCE(powerhal_tsk, NULL);
-}
-
 static LIST_HEAD(formats);
 static DEFINE_RWLOCK(binfmt_lock);
 
@@ -1948,21 +1913,6 @@ static int __do_execve_file(int fd, struct filename *filename,
 
 	if (is_global_init(current->parent)) {
 		android_service_blacklist(filename->name);
-		if (unlikely(!strcmp(filename->name, SERVICEMANAGER_BIN)))
-			WRITE_ONCE(servicemanager_tsk, current);
-		if (unlikely(!strcmp(filename->name, LIBPERFMGR))) {
-			WRITE_ONCE(powerhal_tsk, current);
-                } else if (unlikely(!strcmp(filename->name, LIBPERFMGR_BIN))) {
-                        WRITE_ONCE(powerhal_tsk, current);
-                } else if (unlikely(!strcmp(filename->name, PERF))) {
-                        WRITE_ONCE(powerhal_tsk, current);
-                } else if (unlikely(!strcmp(filename->name, PERFD))) {
-                        WRITE_ONCE(powerhal_tsk, current);
-                } else if (unlikely(!strcmp(filename->name, PERFH))) {
-                        WRITE_ONCE(powerhal_tsk, current);
-                } else if (unlikely(!strcmp(filename->name, IOP))) {
-                        WRITE_ONCE(powerhal_tsk, current);
-		}
 		if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN)))
 			zygote32_sig = current->signal;
 		else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
