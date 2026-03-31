e1c36b41c5b39dea60d314bfc04231349a57e5f4
Sergey Senozhatsky <senozhatsky@chromium.org>
2026-03-31 16:42:44 +0900

    zram: Do not forget to endio for partial discard requests
    As reported by Qu Wenruo and Avinesh Kumar, the following
    
     getconf PAGESIZE
     65536
     blkdiscard -p 4k /dev/zram0
    
    takes literally forever to complete.  zram doesn't support partial
    discards and just returns immediately w/o doing any discard work in such
    cases.  The problem is that we forget to endio on our way out, so
    blkdiscard sleeps forever in submit_bio_wait().  Fix this by jumping to
    end_bio label, which does bio_endio().
    
    Link: https://lkml.kernel.org/r/20260331074255.777019-1-senozhatsky@chromium.org
    Closes: https://lore.kernel.org/linux-block/92361cd3-fb8b-482e-bc89-15ff1acb9a59@suse.com
    Closes: https://bugzilla.suse.com/show_bug.cgi?id=1256530
    Fixes: 0120dd6e4e20 ("zram: make zram_bio_discard more self-contained")
    Change-Id: Ia41e8aee18eafff01b1090aae62e4d98b8cf76df
    Cc: Brian Geffon <bgeffon@google.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Minchan Kim <minchan@kernel.org>
    Reported-by: Qu Wenruo <wqu@suse.com>
    Reported-by: Avinesh Kumar <avinesh.kumar@suse.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Tested-by: Qu Wenruo <wqu@suse.com>
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -2247,7 +2247,7 @@ static void zram_bio_discard(struct zram *zram, struct bio *bio)
 	 */
 	if (offset) {
 		if (n <= (PAGE_SIZE - offset))
-			return;
+			goto end_bio;
 
 		n -= (PAGE_SIZE - offset);
 		index++;
@@ -2262,6 +2262,7 @@ static void zram_bio_discard(struct zram *zram, struct bio *bio)
 		n -= PAGE_SIZE;
 	}
 
+end_bio:
 	bio_endio(bio);
 }
 
