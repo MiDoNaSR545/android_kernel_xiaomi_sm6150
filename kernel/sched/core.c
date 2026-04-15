ea0c42bfa02e0d2856a55a9922cb61fa13c95abc
kondors1995 <normandija1945@gmail.com>
2026-04-15 22:25:55 +0300

    sched:core: Fix sched_setattr
    04-15 22:06:50.111 W/powerhal-libperfmgr(852): sched_setattr failed for thread 5126, err=22
    
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5723,6 +5723,8 @@ SYSCALL_DEFINE3(sched_setattr, pid_t, pid, struct sched_attr __user *, uattr,
 	rcu_read_unlock();
 
 	if (likely(p)) {
+		if (attr.sched_flags & SCHED_FLAG_KEEP_PARAMS)
+			get_params(p, &attr);
 		retval = sched_setattr(p, &attr);
 		put_task_struct(p);
 	}
