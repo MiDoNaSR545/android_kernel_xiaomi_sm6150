04e4b7f791c26a425d9fc124247e2b9301075971
Yahya Wessam <yahyawessam2002@gmail.com>
2026-04-18 15:10:03 +0200

    Revert "sched: use raw capacity scale for uclamp"
    This reverts commit 972410248c4a550f407ba0cc827f6b1e715f1415.
    
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5218,14 +5218,8 @@ recheck:
 	}
 
 	if (attr->sched_flags &
-    ~(SCHED_FLAG_RESET_ON_FORK  |
-      SCHED_FLAG_RECLAIM        |
-      SCHED_FLAG_KEEP_POLICY    |
-      SCHED_FLAG_KEEP_PARAMS    |
-      SCHED_FLAG_UTIL_CLAMP     |
-      SCHED_FLAG_UTIL_CLAMP_MIN |
-      SCHED_FLAG_UTIL_CLAMP_MAX))
-    return -EINVAL;
+		~(SCHED_FLAG_RESET_ON_FORK | SCHED_FLAG_RECLAIM))
+		return -EINVAL;
 
 	/*
 	 * Valid priorities for SCHED_FIFO and SCHED_RR are
@@ -5235,9 +5229,8 @@ recheck:
 	if ((p->mm && attr->sched_priority > MAX_USER_RT_PRIO-1) ||
 	    (!p->mm && attr->sched_priority > MAX_RT_PRIO-1))
 		return -EINVAL;
-	if (!(attr->sched_flags & SCHED_FLAG_KEEP_PARAMS) &&
-	    ((dl_policy(policy) && !__checkparam_dl(attr)) ||
-	    (rt_policy(policy) != (attr->sched_priority != 0))))
+	if ((dl_policy(policy) && !__checkparam_dl(attr)) ||
+	    (rt_policy(policy) != (attr->sched_priority != 0)))
 		return -EINVAL;
 
 	/*
@@ -8333,18 +8326,17 @@ capacity_from_percent(char *buf)
 
 	buf = strim(buf);
 	if (strcmp(buf, "max")) {
-		u64 val;
-
-		req.ret = kstrtoull(buf, 10, &val);
+		req.ret = cgroup_parse_float(buf, UCLAMP_PERCENT_SHIFT,
+					     &req.percent);
 		if (req.ret)
 			return req;
-		if (val > SCHED_CAPACITY_SCALE) {
+		if ((u64)req.percent > UCLAMP_PERCENT_SCALE) {
 			req.ret = -ERANGE;
 			return req;
 		}
 
-		req.util = val;
-		req.percent = DIV_ROUND_CLOSEST_ULL(val * UCLAMP_PERCENT_SCALE, SCHED_CAPACITY_SCALE);
+		req.util = req.percent << SCHED_CAPACITY_SHIFT;
+		req.util = DIV_ROUND_CLOSEST_ULL(req.util, UCLAMP_PERCENT_SCALE);
 	}
 
 	return req;
@@ -8438,6 +8430,8 @@ static inline void cpu_uclamp_print(struct seq_file *sf,
 {
 	struct task_group *tg;
 	u64 util_clamp;
+	u64 percent;
+	u32 rem;
 
 	rcu_read_lock();
 	tg = css_tg(seq_css(sf));
@@ -8449,7 +8443,9 @@ static inline void cpu_uclamp_print(struct seq_file *sf,
 		return;
 	}
 
-	seq_printf(sf, "%llu\n", util_clamp);
+	percent = tg->uclamp_pct[clamp_id];
+	percent = div_u64_rem(percent, POW10(UCLAMP_PERCENT_SHIFT), &rem);
+	seq_printf(sf, "%llu.%0*u\n", percent, UCLAMP_PERCENT_SHIFT, rem);
 }
 
 static int cpu_uclamp_min_show(struct seq_file *sf, void *v)
