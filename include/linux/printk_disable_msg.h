ada0535011ee35f00cf24f2a88f6dfe813f7231e
Yahya Wessam <yahyawessam2002@gmail.com>
2026-04-25 03:00:22 +0300

    Revert "printk_disable_msg: Suppress {qg,smblib}_dbg"
    This reverts commit 78ad57197fee7d1e868fe6778b5d5b7e7b291fea.
    
    Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

--- include/linux/printk_disable_msg.h
+++ include/linux/printk_disable_msg.h
@@ -24,10 +24,4 @@
 #define dev_err(dev, fmt, ...)
 #define dev_err_ratelimited(dev, fmt, ...)
 
-#undef qg_dbg
-#undef smblib_dbg
-
-#define qg_dbg(chip, reason, fmt, ...)
-#define smblib_dbg(chg, reason, fmt, ...)
-
 #endif
