AkiHaza/android_kernel_oneplus_sm8650

sched/cass: respect uclamp max clamping
Background tasks are heavily max clamped traditionally on android.
Allowing max clamped tasks to be placed on prime core when it is
only lower utilization wastes power when a lower power core
could service a max clamped task without issue.

As an example: A task capped to UCLAMP_MAX=300 on the Prime CPU
(capacity 1024) wastes ~70% of Prime's power budget (given
dynamic-power-coefficient) when a Gold CPU (capacity 983) or LITTLE
CPU (capacity 488) would serve it just as well at a lower P-state.

Also clamp eff_util to uc_max, so overload detection no longer
overestimates max clamped tasks.

kernel/sched/cass.c
@@ -98,6 +98,7 @@ bool cass_prime_cpu(const struct cass_cpu_cand *c)
 static __always_inline
 bool cass_cpu_better(const struct cass_cpu_cand *a,
 		     const struct cass_cpu_cand *b, unsigned long p_util,
+		     unsigned long uc_max,
 		     int this_cpu, int prev_cpu, bool sync)
 {
 #define cass_cmp(a, b) ({ res = (a) - (b); })
@@ -121,6 +122,20 @@ bool cass_cpu_better(const struct cass_cpu_cand *a,
 	if (cass_cmp(fits_capacity(p_util, a->cap_max),
 		     fits_capacity(p_util, b->cap_max)))
 		goto done;
+
+	/* Prefer smaller CPUs for tasks capped by uclamp_max below Prime */
+	if (uc_max < SCHED_CAPACITY_SCALE) {
+		bool a_fits = a->cap_orig >= uc_max;
+		bool b_fits = b->cap_orig >= uc_max;
+
+		/* Prefer a CPU that satisfies the cap over one that doesn't */
+		if (cass_cmp(a_fits, b_fits))
+			goto done;
+
+		/* Among CPUs that satisfy the cap, prefer the smaller one */
+		if (a_fits && b_fits && cass_cmp(b->cap_orig, a->cap_orig))
+			goto done;
+	}
 
 	/* Prefer the CPU with higher actual capacity */
 	if (cass_cmp(a->cap_actual, b->cap_actual))
@@ -197,7 +212,7 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 	/* Initialize @best such that @best always has a valid CPU at the end */
 	struct cass_cpu_cand cands[2], *best = cands;
 	int this_cpu = raw_smp_processor_id();
-	unsigned long p_util, uc_min;
+	unsigned long p_util, uc_min, uc_max;
 	bool has_idle = false;
 	int cidx = 0, cpu;
 
@@ -207,6 +222,7 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 	 */
 	p_util = rt ? 0 : task_util_est(p);
 	uc_min = uclamp_eff_value(p, UCLAMP_MIN);
+	uc_max = uclamp_eff_value(p, UCLAMP_MAX);
 
 	/*
 	 * Find the best CPU to wake @p on. Although idle_get_state() requires
@@ -299,7 +315,7 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 		 * overloaded, since the relative utilization calculation
 		 * disregards thermal pressure.
 		 */
-		curr->eff_util = max(curr->util + curr->hard_util, uc_min);
+		curr->eff_util = min(max(curr->util + curr->hard_util, uc_min), uc_max);
 
 		/* Clamp the utilization to the minimum performance threshold */
 		if (curr->util < uc_min)
@@ -327,8 +343,8 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 		 * cidx still needs to be changed to the other candidate slot.
 		 */
 		if (best == curr ||
-		    cass_cpu_better(curr, best, p_util, this_cpu, prev_cpu,
-				    sync)) {
+		    cass_cpu_better(curr, best, p_util, uc_max, this_cpu,
+				    prev_cpu, sync)) {
 			best = curr;
 			cidx ^= 1;
 		}

MiDoNaSR545/android_kernel_xiaomi_sm6150

Signed-off-by: Yahya Wessam <yahyawessam2002@gmail.com>

kernel/sched/cass.c
@@ -76,6 +76,7 @@ void cass_cpu_util(struct cass_cpu_cand *c, int this_cpu, bool sync)
 static __always_inline
 bool cass_cpu_better(const struct cass_cpu_cand *a,
 		     const struct cass_cpu_cand *b, unsigned long p_util,
+		     unsigned long uc_max,
 		     int this_cpu, int prev_cpu, bool sync)
 {
 #define cass_cmp(a, b) ({ res = (a) - (b); })
@@ -98,6 +99,20 @@ bool cass_cpu_better(const struct cass_cpu_cand *a,
 	if (cass_cmp(fits_capacity(p_util, a->cap_max),
 		     fits_capacity(p_util, b->cap_max)))
 		goto done;
+
+	/* Prefer smaller CPUs for tasks capped by uclamp_max below Prime */
+	if (uc_max < SCHED_CAPACITY_SCALE) {
+		bool a_fits = a->cap >= uc_max;
+		bool b_fits = b->cap >= uc_max;
+
+		/* Prefer a CPU that satisfies the cap over one that doesn't */
+		if (cass_cmp(a_fits, b_fits))
+			goto done;
+
+		/* Among CPUs that satisfy the cap, prefer the smaller one */
+		if (a_fits && b_fits && cass_cmp(b->cap, a->cap))
+			goto done;
+	}
 
 	/* Prefer the CPU with higher capacity */
 	if (cass_cmp(a->cap, b->cap))
@@ -160,7 +175,7 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 	/* Initialize @best such that @best always has a valid CPU at the end */
 	struct cass_cpu_cand cands[2], *best = cands;
 	int this_cpu = raw_smp_processor_id();
-	unsigned long p_util, uc_min;
+	unsigned long p_util, uc_min, uc_max;
 	bool has_idle = false;
 	int cidx = 0, cpu;
 
@@ -170,6 +185,7 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 	 */
 	p_util = rt ? 0 : task_util_est(p);
 	uc_min = uclamp_eff_value(p, UCLAMP_MIN);
+	uc_max = uclamp_eff_value(p, UCLAMP_MAX);
 
 	/*
 	 * Find the best CPU to wake @p on. Although idle_get_state() requires
@@ -248,7 +264,7 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 		 * overloaded, since the relative utilization calculation
 		 * disregards thermal pressure.
 		 */
-		curr->eff_util = max(curr->util + curr->hard_util, uc_min);
+		curr->eff_util = min(max(curr->util + curr->hard_util, uc_min), uc_max);
 
 		/* Clamp the utilization to the minimum performance threshold */
 		if (curr->util < uc_min)
@@ -276,8 +292,8 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 		 * cidx still needs to be changed to the other candidate slot.
 		 */
 		if (best == curr ||
-		    cass_cpu_better(curr, best, p_util, this_cpu, prev_cpu,
-				    sync)) {
+		    cass_cpu_better(curr, best, p_util, uc_max, this_cpu,
+				    prev_cpu, sync)) {
 			best = curr;
 			cidx ^= 1;
 		}